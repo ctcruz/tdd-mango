1cee485903551d702f62e017e925cd2991d91cb8
Tarsis Costa Cruz <tarsis@Tarsiss-MacBook-Air.local>
2022-05-07 05:22:05 -0300

    feat: wipipipipip

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -28,7 +28,7 @@ const makeAddAccountRepository = (): AddAccountRepository => {
         id: 'valid_id',
         name: 'valid_name',
         email: 'valid_email',
-        password: 'valid_password'
+        password: 'hashed_password'
       }
       return await new Promise(resolve => resolve(fakeAccount))
     }
@@ -60,21 +60,21 @@ describe('DbAddAccount Usecase', () => {
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
 
-  // test('Should throw if Encrypter throws', async () => {
-  //   const { sut, encrypterStub } = makeSut()
-  //   const encryptSpy = jest
-  //     .spyOn(encrypterStub, 'encrypt')
-  //     .mockReturnValueOnce(
-  //       new Promise((resolve, reject) => reject(new Error()))
-  //     )
-  //   const accountData = {
-  //     name: 'valid_name',
-  //     email: 'valid_email',
-  //     password: 'valid_password'
-  //   }
-  //   const promise = sut.add(accountData)
-  //   await expect(promise).rejects.toThrow('valid_password')
-  // })
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
 
   test('Should call AddAccountRepository with correct values', async () => {
     const { sut, addAccountRepositoryStub } = makeSut()
@@ -86,6 +86,38 @@ describe('DbAddAccount Usecase', () => {
     }
     await sut.add(accountData)
     expect(addSpy).toHaveBeenCalledWith({
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
+
+  test('Should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest
+      .spyOn(addAccountRepositoryStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      )
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const promise = sut.add(accountData)
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
       name: 'valid_name',
       email: 'valid_email',
       password: 'hashed_password'
