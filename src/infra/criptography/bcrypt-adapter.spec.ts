7b052d4a30bf43101eaa0248481f5333762002f5
Tarsis Cruz <tarsis.cruz@avenuecode.com>
2022-05-11 05:58:56 -0300

    chore: add jest configs for mongodb

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -2,8 +2,8 @@ import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
-  async hash (): Promise <string> {
-    return await new Promise(resolve => resolve('hash'))
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash'))
   }
 }))
 
